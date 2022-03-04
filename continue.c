9b9d95383c14fac4f24e5f98996a07de0e9eca02
rajdeep-234 <89674662+rajdeep-234@users.noreply.github.com>
2022-03-04 19:44:40 +0530

    Add files via upload

---
+++ continue.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    char country[5]="India";
+    char currency='$';//Currency is in US Dollar
+    float GDP=2.2;//here GDP GREW US$2.2 TRILLION
+    int year=2015;//The year was 2015
+    printf("The GDP Of %s grew five fold to reach US%c%.1f trillion in %d\n",country,currency,GDP,year);
+    return 0;
+}
